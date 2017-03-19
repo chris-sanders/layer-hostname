c159df62c8e460fddb6ef634630a357b86232bdc
Chris Sanders <sanders.chris@gmail.com>
2017-03-19 14:11:13 -0500

    Initial commit

---
+++ reactive/hostname.py
@@ -0,0 +1,20 @@
+from charms.reactive import when, when_not, set_state
+from charmhelpers.core import hookenv
+import subprocess
+
+@when('config.changed.hostname')
+def update_hostname():
+    config = hookenv.config()
+    hostname = config['hostname'] 
+    if hostname is not 'defaultHostName':
+        with open('/etc/hostname','w') as file:
+            file.write(hostname)
+        subprocess.call(['sudo','hostname',hostname])
+        # Need to replace /etc/hosts line as well
+ 
+@when_not('layer-hostname.installed')
+def install_layer_hostname():
+   update_hostname()
+   set_state('layer-hostname.installed')
+
+
